6115cbb9a63cac515cea83d2ec772547d5748396
robertdancos33 <97561925+robertdancos33@users.noreply.github.com>
2025-02-06 02:52:19 +0200

    Delete buildAndRun.sh

--- buildAndRun.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mvn clean package && docker rm -f fitness-shop.dev ||
-    docker rm -f fitness-shop-mysql.dev || 
-    docker rmi patrickhub/fitness-shop:1.0-SNAPSHOT ||
-    true && docker-compose up
